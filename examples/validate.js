4ea85120e76d7e56071b2b06d702c16a2deb3fc4
Ryan Conigliaro <rmconigliaro@gmail.com>
2023-01-14 22:03:46 -0500

    updating version

--- examples/validate.js
+++ examples/validate.js
@@ -1,4 +1,4 @@
-import { createRequire } from "module";
+import { createRequire } from 'module';
 import { DerefSchema } from '../dist/index.js';
 
 const require = createRequire(import.meta.url);
@@ -11,8 +11,8 @@ const feedSchemaDeref = new DerefSchema(feedSchema);
 
 const result = feedSchemaDeref.getValidator().validate({
     data: [
-        { id: "1", price: { units: 100 }, time: { seconds: 100 } },
-        { id: "1", price: { units: 100, currency: 'USD' }, time: { seconds: 200 } }
+        { id: '1', price: { units: 100 }, time: { seconds: 100 } },
+        { id: '1', price: { units: 100, currency: 'USD' }, time: { seconds: 200 } }
     ]
 });
 
@@ -26,13 +26,13 @@ const feedItemSchemaDeref= new DerefSchema(
     shortCircuit);
 
 const feedItemResult = feedItemSchemaDeref.getValidator().validate(
-    { id: "1", price: { units: 100 }, time: { seconds: 100 } },
+    { id: '1', price: { units: 100 }, time: { seconds: 100 } },
 );
 console.log('feedItem result = ', getErrors(feedItemResult));
 
 function getErrors(result) {
     return result.errors && result.errors.filter((item) => {
-        return !["$ref", "items", "properties"].includes(item.keyword);
+        return !['$ref', 'items', 'properties'].includes(item.keyword);
     }).map((item)=> {
         return `instanceLocation: ${item.instanceLocation}, error: ${item.error}`;
     });
