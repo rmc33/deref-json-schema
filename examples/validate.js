c1c3b7ed00b93fd7dfe7f3fd8bd5113bb820fbf0
Ryan Conigliaro <rmconigliaro@gmail.com>
2023-01-14 21:21:32 -0500

    moving all functions to DerefSchema class

--- examples/validate.js
+++ examples/validate.js
@@ -1,6 +1,5 @@
-import { Validator } from '@cfworker/json-schema';
 import { createRequire } from "module";
-import { DerefSchema, DerefValidator } from '../dist/index.js';
+import { DerefSchema } from '../dist/index.js';
 
 const require = createRequire(import.meta.url);
 const feedSchema = require('./feed.schema.json');
@@ -8,9 +7,9 @@ const feedSchema = require('./feed.schema.json');
 const shortCircuit = false;
 const draft = '2020-12';
 
-const validator = new DerefValidator(feedSchema);
+const feedSchemaDeref = new DerefSchema(feedSchema);
 
-const result = validator.getValidator().validate({
+const result = feedSchemaDeref.getValidator().validate({
     data: [
         { id: "1", price: { units: 100 }, time: { seconds: 100 } },
         { id: "1", price: { units: 100, currency: 'USD' }, time: { seconds: 200 } }
@@ -21,12 +20,12 @@ console.log(JSON.stringify(feedSchema));
 
 console.log('result = ', getErrors(result));
 
-const feedItemValidator = new DerefValidator(
+const feedItemSchemaDeref= new DerefSchema(
     { $ref: '#/definitions/feed_item', definitions: feedSchema.definitions },
     draft,
     shortCircuit);
 
-const feedItemResult = feedItemValidator.getValidator().validate(
+const feedItemResult = feedItemSchemaDeref.getValidator().validate(
     { id: "1", price: { units: 100 }, time: { seconds: 100 } },
 );
 console.log('feedItem result = ', getErrors(feedItemResult));
