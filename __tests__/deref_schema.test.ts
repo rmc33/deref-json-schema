bea5b78dc06e2ffe942745d143bbc3340521991f
Ryan Conigliaro <rmconigliaro@gmail.com>
2023-01-24 23:35:40 -0500

    update to tests

--- __tests__/deref_schema.test.ts
+++ __tests__/deref_schema.test.ts
@@ -1,42 +1,55 @@
 import {DerefSchema} from '../src/deref_schema';
+import { Schema } from '@cfworker/json-schema';
 import fs from 'fs';
 
 describe('DerefSchema module', () => {
 
   describe('calling create', () => {
 
-    const schema = {
-        $ref: 'test.json',
-        $id: "https://deref_schema.com/schema.json"
-    };
+    let schema: Schema;
+    let subSchema: Schema;
 
-    test('calling create with $ref reads subschema', () => {
-        const subSchema = {
+    beforeEach(()=> {
+        schema = {
+            $ref: 'test.json',
+            $id: "https://deref_schema.com/schema.json"
+        };
+        subSchema = {
             type: "number",
             $id: "https://deref_schema.com/subschema.json"
         };
-        //(fs.readFileSync as Mock).mockReturnValue(JSON.stringify(subSchema));
+    });
+
+    test('calling create with $ref reads subschema', () => {
         jest.spyOn(fs, 'readFileSync').mockReturnValue(JSON.stringify(subSchema));
-        const derefSchema = DerefSchema.create(schema);
+        const derefSchema: DerefSchema = DerefSchema.create(schema);
+        expect(fs.readFileSync).toHaveBeenCalledWith('test.json', 'utf-8');
+        expect(derefSchema.getSchemasAded()).toEqual(new Set(['test.json']));
+    });
+
+    test('calling create with $ref reads subschema when ref value has #/definitions', () => {
+        schema['$ref'] = 'test.json#/definitions/test';
+        jest.spyOn(fs, 'readFileSync').mockReturnValue(JSON.stringify(subSchema));
+        const derefSchema: DerefSchema = DerefSchema.create(schema);
         expect(fs.readFileSync).toHaveBeenCalledWith('test.json', 'utf-8');
         expect(derefSchema.getSchemasAded()).toEqual(new Set(['test.json']));
     });
 
     test('calling create with $ref adds subschema from subschema', () => {
-        const subSchema = {
+        const subSchema: Schema = {
             $id: "https://deref_schema.com/subschema.json",
             definitions: {
                 example: {
                     type: "boolean"
                 },
                 other: {
-                    $ref: "other.json#/definitions/other"
+                    $ref: "other.json"
                 }
             }
         };
         jest.spyOn(fs, 'readFileSync').mockReturnValueOnce(JSON.stringify(subSchema))
             .mockReturnValue(JSON.stringify({type: 'number'}));
-        const derefSchema = DerefSchema.create(schema);
+        const derefSchema: DerefSchema = DerefSchema.create(schema);
         expect(fs.readFileSync).toHaveBeenCalledWith('test.json', 'utf-8');
         expect(fs.readFileSync).toHaveBeenCalledWith('other.json', 'utf-8');
         expect(derefSchema.getSchemasAded()).toEqual(new Set(['test.json', 'other.json']));
